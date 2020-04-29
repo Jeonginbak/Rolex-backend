a3b2003645c532716f1b775d606bea3174c1048a
pyjjin <pyheejin@gmail.com>
2020-04-30 00:28:49 +0900

    좋아요 미리보기 데이터 엔드포인트 수정

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SignUp, LogIn, LikeView, MyLikeListPreview, MyLikeListDetail
+from .views import SignUp, LogIn, LikeView, MyLikeListPreview
 
 
 urlpatterns = [
@@ -8,5 +8,4 @@ urlpatterns = [
     path('/sign-up', SignUp.as_view()),
 	path('/like', LikeView.as_view()),
 	path('/mylike/preview', MyLikeListPreview.as_view()),
-	path('/mylike/detail', MyLikeListDetail.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -44,7 +44,6 @@ class LogIn(View):
 		except KeyError:
 			return JsonResponse({'message':'invalid'}, status=401)
 
-
 class LikeView(View):
 	@login_decorator
 	def post(self, request, *args, **kwargs):
@@ -61,84 +60,23 @@ class LikeView(View):
 				Like.objects.create(product_id=product_id, user_id=user.id)
 		return HttpResponse(status=200)
 
-
 class MyLikeListPreview(View):
 	@login_decorator
 	def get(self, request):
 		user = request.user
 		like_list = Like.objects.filter(user=user.id)
-		data_list = list(like_list)
+	
+		for data in like_list:
+			product = Product.objects.get(id=data.product_id)
+			detail = Detail.objects.get(id=data.product_id)
+			middle = MiddleImage.objects.get(id=data.product_id)
+			feature = Feature.objects.get(id=data.product_id)
 
-		for data in data_list:
-			collection = Product.objects.get(id=data.product_id).collection.name
-			image = Product.objects.get(id=data.product_id).header_watch
-			oyster = Detail.objects.get(id=data.product_id).is_oyster
-			size = Detail.objects.get(id=data.product_id).size.diameter
-			material = Detail.objects.get(id=data.product_id).material.name
-		
-		result = {
-			'collection':collection,
-			'image':image,
-			'oyster':oyster,
-			'size':size,
-			'material':material
-		}	
-		
-		return JsonResponse({'product_preview':result}, status=200)
-
-
-
-class MyLikeListDetail(View):
-	@login_decorator
-	def get(self, request):
-		user = request.user
-		like_list = Like.objects.filter(user=user.id)
-		data_list = list(like_list)
-
-		for data in data_list:
-			category = Product.objects.get(id=data.product_id).category.name
-			collection = Product.objects.get(id=data.product_id).collection.name
-			image = Product.objects.get(id=data.product_id).header_watch
-			background_image = Product.objects.get(id=data.product_id).header_background
-			oyster = Detail.objects.get(id=data.product_id).is_oyster
-			size = Detail.objects.get(id=data.product_id).size.diameter
-			material = Detail.objects.get(id=data.product_id).material.name
-			price = Detail.objects.get(id=data.product_id).price
-			description = Product.objects.get(id=data.product_id).description
-			sub_description = Product.objects.get(id=data.product_id).sub_description
-			middle_thumbnail_image = MiddleImage.objects.get(id=data.product_id).thumbnail_url
-			middle_image = MiddleImage.objects.get(id=data.product_id).image_url
-			middle_title = MiddleImage.objects.get(id=data.product_id).title
-			middle_sub_title = MiddleImage.objects.get(id=data.product_id).sub_title
-			middle_description = MiddleImage.objects.get(id=data.product_id).description
-			feature_title = Feature.objects.get(id=data.product_id).title
-			feature_sub_title = Feature.objects.get(id=data.product_id).sub_title
-			feature_description = Feature.objects.get(id=data.product_id).description
-			feature_thumbnail_image = Feature.objects.get(id=data.product_id).thumbnail_url
-			feature_image = Feature.objects.get(id=data.product_id).image_url
-
-		
-		result = {
-			'category':category,
-			'collection':collection,
-			'image':image,
-			'background_image':background_image,
-			'oyster':oyster,
-			'size':size,
-			'material':material,
-			'price':price,
-			'description':description,
-			'sub_descritpion':sub_description,
-			'middle_thumbnail_image':middle_thumbnail_image,
-			'middle_image':middle_image,
-			'middle_title':middle_title,
-			'middle_sub_title':middle_sub_title,
-			'middle_description':middle_description,
-			'feature_title':feature_title,
-			'feature_sub_title':feature_sub_title,
-			'feature_description':feature_description,
-			'feature_thumbnail_image':feature_thumbnail_image,
-			'feature_image':feature_image
-		}	
-		
-		return JsonResponse({'product_detail':result}, status=200)
+			data_attribute = {
+				'collection':product.collection.name,
+				'thumbnail_image':product.header_watch,
+				'oyster':detail.is_oyster,
+				'size':detail.size.diameter,
+				'material':detail.material.name,
+			}
+		return JsonResponse({'product_preview':data_attribute}, status=200)
